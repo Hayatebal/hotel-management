829ba0e79de9141f381025818cb708251ca88118
Marian <hayatebal@gmail.com>
2026-05-28 01:32:32 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\GuestController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return redirect('/login');
@@ -17,23 +18,59 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
-    Route::get('/profile', function () {
-        return view('profile');
-    })->name('profile.edit');
+    /*
+    |--------------------------------------------------------------------------
+    | PROFILE ROUTES
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+
+    /*
+    |--------------------------------------------------------------------------
+    | RESOURCE ROUTES
+    |--------------------------------------------------------------------------
+    */
 
     Route::resource('rooms', RoomController::class);
+
     Route::resource('guests', GuestController::class);
+
     Route::resource('reservations', ReservationController::class);
+
     Route::resource('payments', PaymentController::class);
 
-    Route::post('/reservations/{id}/checkin', [ReservationController::class, 'checkin'])
+    /*
+    |--------------------------------------------------------------------------
+    | RESERVATION ROUTES
+    |--------------------------------------------------------------------------
+    */
+
+    Route::post('/reservations/{id}/checkin',
+        [ReservationController::class, 'checkin'])
         ->name('reservations.checkin');
 
-    Route::get('/reservations/{reservation}/checkin-receipt', [ReservationController::class, 'checkinReceipt'])
+    Route::get('/reservations/{reservation}/checkin-receipt',
+        [ReservationController::class, 'checkinReceipt'])
         ->name('reservations.checkinReceipt');
 
-    Route::get('/payments/{payment}/checkout-receipt', [PaymentController::class, 'checkoutReceipt'])
+    /*
+    |--------------------------------------------------------------------------
+    | PAYMENT ROUTES
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/payments/{payment}/checkout-receipt',
+        [PaymentController::class, 'checkoutReceipt'])
         ->name('payments.checkoutReceipt');
+
 });
 
 require __DIR__.'/auth.php';
